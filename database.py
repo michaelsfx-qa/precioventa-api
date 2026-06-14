b42d9c41cd0119dfda77614fe452eee2cd8c762c
michaelsfx-qa <plusultrasfx1@gmail.com>
2026-06-14 16:37:54 -0400

    variables de entorno para base de datos [skip ci]

--- database.py
+++ database.py
@@ -1,9 +1,13 @@
 import psycopg2
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def get_connection():
     return psycopg2.connect(
-        host="localhost",
-        database="precioventa_db",
-        user="postgres",
-        password="248710"
+        host=os.getenv("DB_HOST"),
+        database=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD")
     )
